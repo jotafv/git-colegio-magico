594c256a461f95126ff26c367c5b3eb36770bcbe
jota_00ms <juanjoseflorezvalencia2009@gmail.com>
2026-09-05 16:31:10 +0000

    :feat:Se implemento la funcion preparar para el reto 3

--- src/main/java/com/colegiomagico/reto3/PocionCiclos.java
+++ src/main/java/com/colegiomagico/reto3/PocionCiclos.java
@@ -4,9 +4,12 @@ package com.colegiomagico.reto3;
  * Usa un ciclo for para crear una cadena que repita la palabra 'Pocion ' la cantidad de veces indicada.
  */
 public class PocionCiclos {
-
     public static String preparar(int cantidad) {
-        return ""; // TODO: Cambia esto
+        String resultado ="";
+        for (int i = 0; i < cantidad; i++) {
+            resultado += "Pocion ";
+        }
+        return resultado;
     }
 
     public static void main(String[] args) {
